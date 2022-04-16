a1de9a1504ede3be85108cb49f042c8d8df27da6
Igor Chernyshov <igorchernyshov@yandex.ru>
2022-04-16 14:23:52 +0300

    hw02-generics executed

--- hw02-generics/src/main/java/homework/Customer.java
+++ hw02-generics/src/main/java/homework/Customer.java
@@ -49,16 +49,24 @@ public class Customer {
 
         Customer customer = (Customer) o;
 
+        return id == customer.id;
+
+/*
         if (id != customer.id) return false;
         if (scores != customer.scores) return false;
         return name != null ? name.equals(customer.name) : customer.name == null;
+ */
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
-        return result;
+        return Long.hashCode(id);
+
+        /*
+            int result = (int) (id ^ (id >>> 32));
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            result = 31 * result + (int) (scores ^ (scores >>> 32));
+            return result;
+        */
     }
 }
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,59 @@
 package homework;
 
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
-
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
+    private final TreeMap<Customer, String> customers =
+            new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
+
+
+    private static class Node implements Map.Entry<Customer, String> {
+        final private Customer key;
+        private String value;
+
+        @Override
+        public Customer getKey() {
+            return key;
+        }
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String setValue(String value) {
+            this.value = value;
+            return value;
+        }
+
+        public Node(Customer key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = customers.firstEntry();
+        return entry == null ? null :
+                new Node( new Customer( entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()), entry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = customers.higherEntry(customer);
+        return entry == null ? null :
+                new Node( new Customer( entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()), entry.getValue());
     }
 
     public void add(Customer customer, String data) {
-
+        customers.put(customer, data);
     }
 }
