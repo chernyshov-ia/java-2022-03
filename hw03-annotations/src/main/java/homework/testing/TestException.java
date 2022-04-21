ac504739b8000de0bc3ecd6cf5e6b542f9ea2a39
Igor Chernyshov <igorchernyshov@yandex.ru>
2022-04-21 09:48:36 +0300

    hw03-annotations refactored

--- hw03-annotations/src/main/java/homework/testing/TestException.java
+++ hw03-annotations/src/main/java/homework/testing/TestException.java
@@ -1,10 +1,6 @@
 package homework.testing;
 
 public class TestException extends RuntimeException {
-    public TestException(String message) {
-        super(message);
-    }
-
     public TestException(String message, Throwable cause) {
         super(message, cause);
     }
--- hw03-annotations/src/main/java/homework/testing/TestUtils.java
+++ hw03-annotations/src/main/java/homework/testing/TestUtils.java
@@ -48,7 +48,7 @@ public class TestUtils {
         return list;
     }
 
-    private static void finalizeTest(List<Method> after, Object instance) {
+    private static void finalizeInstance(List<Method> after, Object instance) {
         try {
             for (Method method : after) {
                 System.out.println("[" + instance + "] After: doing <" + method.getName() + ">");
@@ -61,84 +61,101 @@ public class TestUtils {
         }
     }
 
+    private static void prepareInstance(List<Method> before, Object instance) throws Exception {
+        for (Method beforeMethod : before) {
+            System.out.println("[" + instance + "] Before: doing <" + beforeMethod.getName() + ">");
+            beforeMethod.invoke(instance);
+            System.out.println("[" + instance + "] Before: completed <" + beforeMethod.getName() + ">");
+        }
+    }
+
+    private static boolean runInstanceTest(TestMethod testMethod, Object instance) {
+        String testName = (testMethod.displayName != null) &&
+                !"".equals(testMethod.displayName) ? testMethod.displayName : testMethod.method.getName();
+        try {
+            System.out.println("[" + instance + "] Test <" + testName + "> running");
+            testMethod.method.invoke(instance);
+            System.out.println("[" + instance + "] Test <" + testName + "> succeeded");
+            return true;
+        } catch (Exception e) {
+            System.out.println("[" + instance + "] Test <" + testName + "> failed");
+            e.printStackTrace();
+
+        }
+        return false;
+    }
+
     private static boolean runTest(List<Method> before, List<Method> after, TestMethod testMethod, Constructor<?> constructor) {
+        Object instance = newInstance(constructor);
+
+        Exception beforeException = null;
+        try {
+            prepareInstance(before, instance);
+        } catch (Exception e) {
+            System.out.println("[" + instance + "] Before: " + e.getCause().getMessage());
+            beforeException = e;
+        }
+
+        boolean result = false;
+
+        if (beforeException == null) {
+            result = runInstanceTest(testMethod, instance);
+        }
+
+        finalizeInstance(after, instance);
+
+        if (beforeException != null) {
+            throw new TestException("Can't init object", beforeException);
+        }
+
+        return result;
+    }
+
+    private static Object newInstance(Constructor<?> constructor) {
         Object instance;
         try {
             instance = constructor.newInstance();
         } catch (Exception e) {
             throw new TestException("Can't instantiate object", e);
         }
-
-        Exception beforeException = null;
-        try {
-            for (Method beforeMethod : before) {
-                System.out.println("[" + instance + "] Before: doing <" + beforeMethod.getName() + ">");
-                beforeMethod.invoke(instance);
-                System.out.println("[" + instance + "] Before: completed <" + beforeMethod.getName() + ">");
-            }
-        } catch (Exception e) {
-            System.out.println("[" + instance + "] Before: " + e.getCause().getMessage());
-            beforeException = e;
-        }
-
-        if (beforeException != null) {
-            finalizeTest(after, instance);
-            throw new TestException("Can't init object", beforeException);
-        }
-
-        boolean result = false;
-
-        String testName = (testMethod.displayName != null) &&
-                !"".equals(testMethod.displayName) ? testMethod.displayName : testMethod.method.getName();
-        try {
-            System.out.println("[" + instance + "] Test <" + testName + "> running");
-            testMethod.method.invoke(instance);
-            result = true;
-            System.out.println("[" + instance + "] Test <" + testName + "> succeeded");
-        } catch (Exception e) {
-            System.out.println("[" + instance + "] Test <" + testName + "> failed");
-            e.printStackTrace();
-        }
-
-        finalizeTest(after, instance);
-
-        return result;
+        return instance;
     }
 
     private static void showResults(int total, int succeeded, int failed) {
         System.out.printf("Succeeded: %d, failed: %d, total: %d%n", succeeded, failed, total);
     }
 
-    public static void runTests(Class<?> clazz) {
+    private static Constructor<?> getConstructor(Class<?> clazz) {
         Constructor<?> constructor;
         try {
             constructor = clazz.getConstructor();
         } catch (NoSuchMethodException e) {
             throw new TestException("Can't instantiate object", e);
         }
+        return constructor;
+    }
+
+    public static void runTests(Class<?> clazz) {
+        Constructor<?> constructor = getConstructor(clazz);
         List<Method> before = getAnnotatedMethods(clazz, Before.class);
         List<Method> after = getAnnotatedMethods(clazz, After.class);
-        List<TestMethod> tests = getTestMethods(clazz);
 
         int total = 0;
         int succeeded = 0;
-        int failed = 0;
 
         try {
-            for (TestMethod test : tests) {
+            for (TestMethod test : getTestMethods(clazz)) {
                 if (runTest(before, after, test, constructor)) {
                     succeeded++;
-                } else {
-                    failed++;
                 }
                 total++;
             }
         } catch (Exception e) {
-            showResults(total, succeeded, failed);
+            showResults(total, succeeded, total - succeeded);
             throw new TestException(e.getMessage(), e);
         }
 
-        showResults(total, succeeded, failed);
+        showResults(total, succeeded, total - succeeded);
     }
 
     private record TestMethod(String displayName, Method method) {
