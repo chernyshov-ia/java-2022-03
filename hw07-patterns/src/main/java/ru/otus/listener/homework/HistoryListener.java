485f061dbe4c60678ab6f1a05243445ae24b3376
Igor Chernyshov <igorchernyshov@yandex.ru>
2022-06-14 17:08:30 +0300

    hw07 HistoryListener

--- hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,17 +2,23 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.Stack;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private HashMap<Long, Message> history = new HashMap<>();
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        history.put(msg.getId(), msg.toBuilder().build());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(history.get(id));
     }
 }
